a6af49d3bf77685b79dbcb5b8f6974a754ea40a4
Leo Vornberger <leo.vornberger@gmx.de>
2024-02-15 17:21:29 +0100

    Add llh2ecef file

---
+++ llh2ecef.py
@@ -0,0 +1,41 @@
+import math
+import numpy as np
+
+def metersToMiles(lengthInMeters):
+    return lengthInMeters / 1852.0
+
+def milesToMeters(lengthInMiles):
+    return lengthInMiles * 1852.0
+
+def feetToMeters(feet):
+    return feet * 0.3048
+
+def degToRad(deg):
+    return deg / 180.0 * math.pi
+
+def radToDeg(rad):
+    return rad / math.pi * 180.0
+
+earthRadius = 6371000 # meters
+sigma_dme = milesToMeters(0.2)/2 # two sigmas (95%) would be 0.2 NM
+sigma_altimeter = feetToMeters(100) / 2 # two sigmas (95%) would be 100 ft
+
+# lat/lon/height (deg, m amsl) to ecef (m)
+def llh2ecef(llh):
+    r = llh[2] + earthRadius
+    x = r * math.cos(degToRad(llh[0])) * math.cos(degToRad(llh[1]))
+    y = r * math.cos(degToRad(llh[0])) * math.sin(degToRad(llh[1]))
+    z = r * math.sin(degToRad(llh[0]))
+    return np.array([x, y, z])
+
+# ecef (m) to lat/lon/height (deg, m amsl)
+def ecef2llh(ecef):
+    if ecef[0] == 0 and ecef[1] == 0:
+        if ecef[2] > 0: lat = 90
+        elif ecef[2] < 0: lat = -90
+        else: lat = 0
+    else:
+        lat = radToDeg(math.atan(ecef[2] / math.sqrt(ecef[0]*ecef[0]+ecef[1]*ecef[1])))
+    lon = radToDeg(math.atan2(ecef[1], ecef[0]))
+    height = math.sqrt(ecef[0]*ecef[0]+ecef[1]*ecef[1]+ecef[2]*ecef[2]) - earthRadius
+    return np.array([lat, lon, height])
