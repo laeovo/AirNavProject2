1e2b20bf50101fea77b70f2ceac4004fddbf038b
Leo Vornberger <leo.vornberger@gmx.de>
2024-02-24 18:08:43 +0100

    Commence work on velocity estimation

--- gnss_velocity_estimation.py
+++ gnss_velocity_estimation.py
@@ -15,102 +15,42 @@ raw = raw[raw[:, 1] == 0] # only GPS data
 raw = raw[raw[:, 5] == 0] # only L1 band
 raw = raw[raw[:, 8] >= 3] # only data points with tracking status >= 3
 
+# make sure the measurements are sorted by time
+for i in range(len(raw)-1):
+    if raw[i+1, 0] - raw[i, 0] >= 0: continue
+    print("Measurements are not in timely order")
+    exit(1)
+
 ephem = np.loadtxt("group01_ephem.dat")
 
-positions = np.array([])
+previous_tor = -1
+current_tor = 0
+satellites_previous = []
+satellites_current = []
+for raw_row in range(len(raw)):
+    if raw[raw_row, 0] == current_tor: continue
+    previous_tor = current_tor
+    current_tor = raw_row[raw_row, 0]
 
-print(len(np.unique(raw[:,0])), "different times")
+    # identify satellites common between previous and current tor
+    satellites_previous = satellites_current
+    satellites_current = raw[raw[:, 0] == current_tor][:,2]
+    common_satellite_ids = np.intersect1d(satellites_previous, satellites_current)
 
-for tor in np.unique(raw[:,0]):
-    print("Time:", tor)
-    data_points = raw[raw[:, 0] == tor]
-    if len(data_points) < 4: continue # need at least four measurements to determine user position
+    # compute single difference between common satellites
+    delta_Phi = np.zeros(len(common_satellite_ids))
+    for i in range(len(common_satellite_ids)):
+        delta_Phi[i] = raw[raw[:, 0] == current_tor and raw[:,2] == common_satellite_ids[i]] - raw[raw[:, 0] == previous_tor and raw[:,2] == common_satellite_ids[i]]
 
-    satellites = np.array([])
-    z = np.array([])
-    for row in range(len(data_points)):
-        datarow = data_points[row]
-        current_gps_time = tor
-        svid = datarow[2]
-        pr = datarow[3]
-
-        # compute time of transmission (tot)
-        tot_first_guess = current_gps_time - pr / vlight
-        (svpos, svclock, ecode) = ephcal(tot_first_guess, ephem, svid)
-        tot_updated = tot_first_guess - svclock
-
-        # with new tot, compute position of satellite
-        (svpos, svclock, ecode) = ephcal(tot_updated, ephem, svid)
-
-        # apply svclock error to pseudorange
-        pr += svclock*vlight
-
-        # correct for Sagnac effect
-        delta_t = pr/vlight
-        alpha = delta_t * eanvel
-        delta_svpos = np.zeros((3, 1))
-        svpos = np.array([[math.cos(alpha), math.sin(alpha), 0],
-                          [-math.sin(alpha), math.cos(alpha), 0],
-                          [0, 0, 1]]) @ svpos
-
-        if len(satellites) == 0:
-            satellites = np.array(svpos.T)
-            z = np.array([pr])
-        else:
-            satellites = np.concatenate((satellites, svpos.T), axis=0)
-            z = np.concatenate((z, [pr]), axis=0)
-
-    # now that we see at least four satellites, we can compute an actual user position
-    # x_hat is the current position estimate; last component is the distance error produced by the user clock (c * delta t_u)
-    x_hat = np.array([0, 0, 0, 0])
-
-    size_of_last_step = 100000 # arbitrary large value to get the while loop going
-    counter = 0
-    while size_of_last_step > 0.3 and counter < 2000:
-        counter += 1
-
-        z_hat = np.zeros(satellites.shape[0])
-        for i in range(satellites.shape[0]):
-            z_hat[i] = np.linalg.norm(x_hat[0:3] - satellites[i]) + x_hat[3]
-
-        # how much are we off from what we actually measured?
-        delta_z_hat = z - z_hat
-
-        # compute H matrix based on current estimate
-        H = np.zeros((satellites.shape[0], 4))
-        for i in range(len(H)):
-            H[i, 0:3] = (x_hat[0:3] - satellites[i]) / np.linalg.norm(x_hat[0:3] - satellites[i])
-            H[i, 3] = 1
-
-        # compute step into the "right" direction
-        delta_x_hat = np.linalg.inv(H.T @ H) @ H.T @ delta_z_hat
-        size_of_last_step = np.linalg.norm(delta_x_hat)
-
-        # update estimate
-        x_hat = x_hat + delta_x_hat
-
-    new_position = np.zeros((1, 5))
-    new_position[0, 0] = tor
-    new_position[0, 1:4] = ecef2llh(x_hat[0:3])
-    new_position[0, 4] = x_hat[3]
-    if len(positions) == 0: positions = new_position
-    else: positions = np.concatenate((positions, new_position), axis=0)
-
+    # calculate unit vectors to satellites at current time
+    e_current = np.zeros((len(common_satellite_ids), 3))
+    for i in range(len(common_satellite_ids)):
+        e_current[i,:] = 
 
 plt.scatter(positions[:,2], positions[:,1], c=positions[:,3])
 plt.colorbar()
 plt.xlabel("Longitude (degrees)")
 plt.ylabel("Latitude (degrees)")
 plt.title("Estimated positions in lat/lon/height (deg/deg/m)")
-
-# plt.plot(positions[:,0], positions[:,3])
-# plt.ylabel("Height in m")
-# plt.xlabel("Time of Reception")
-# plt.title("Estimated height over time")
-
-# plt.scatter(positions[:,0], positions[:,4])
-# plt.ylabel("User clock offset [m]")
-# plt.xlabel("Time of Reception")
-# plt.title("Estimated error over time")
 plt.show()
 
