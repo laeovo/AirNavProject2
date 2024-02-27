8ca20ae5745023c83a4dcf253d0ccbacacffe229
Leo Vornberger <leo.vornberger@gmx.de>
2024-02-27 22:17:11 +0100

    Further refactor position estimation, needs fix

--- gnss_position_estimation.py
+++ gnss_position_estimation.py
@@ -10,46 +10,30 @@ vlight = 299792458.0 # m/s
 # WGS-84 value for earth rotation rate
 eanvel = 7.2921151467e-05 # rad/s
 
-# returns user position given GPS data that were received at the same time
-def calculateUserPosition(satelliteData):
-    if len(np.unique(satelliteData[:, 0])) != 1:
-        print("Fault in satelliteData: rows contain signals received at different times")
-        return False
+# calculates space vehicle position, given time-of-reception, single pseudorange, SV ID and ephem data
+def calculate_sv_pos(time_of_reception, pr, svid, ephem):
+    # compute time of transmission (tot)
+    tot_first_guess = time_of_reception - pr / vlight
+    (svpos, svclock, ecode) = ephcal(tot_first_guess, ephem, svid)
+    tot_updated = tot_first_guess - svclock
 
-    tor = satelliteData[0, 0] # since all the times are the same, pick the first one.
-    satellites = np.array([])
-    z = np.array([])
-    for row in range(len(satelliteData)):
-        datarow = satelliteData[row]
-        svid = datarow[2]
-        pr = datarow[3]
+    # with new tot, compute position of satellite
+    (svpos, svclock, ecode) = ephcal(tot_updated, ephem, svid)
 
-        # compute time of transmission (tot)
-        tot_first_guess = tor - pr / vlight
-        (svpos, svclock, ecode) = ephcal(tot_first_guess, ephem, svid)
-        tot_updated = tot_first_guess - svclock
+    # apply svclock error to pseudorange
+    pr += svclock * vlight
 
-        # with new tot, compute position of satellite
-        (svpos, svclock, ecode) = ephcal(tot_updated, ephem, svid)
+    # correct for Sagnac effect
+    delta_t = pr / vlight
+    alpha = delta_t * eanvel
+    svpos = np.array([[math.cos(alpha), math.sin(alpha), 0],
+                      [-math.sin(alpha), math.cos(alpha), 0],
+                      [0, 0, 1]]) @ svpos
 
-        # apply svclock error to pseudorange
-        pr += svclock * vlight
+    return svpos
 
-        # correct for Sagnac effect
-        delta_t = pr / vlight
-        alpha = delta_t * eanvel
-        svpos = np.array([[math.cos(alpha), math.sin(alpha), 0],
-                          [-math.sin(alpha), math.cos(alpha), 0],
-                          [0, 0, 1]]) @ svpos
-
-        if len(satellites) == 0:
-            satellites = np.array(svpos.T)
-            z = np.array([pr])
-        else:
-            satellites = np.concatenate((satellites, svpos.T), axis=0)
-            z = np.concatenate((z, [pr]), axis=0)
-
-    # now that we see at least four satellites, we can compute an actual user position
+# calculates user position in ECEF and user clock error, given pseudoranges and sv positions
+def calculate_least_squares_solution(pseudoranges, satellitePositions):
     # x_hat is the current position estimate; last component is the distance error produced by the user clock (c * delta t_u)
     x_hat = np.array([0, 0, 0, 0])
 
@@ -58,17 +42,17 @@ def calculateUserPosition(satelliteData):
     while size_of_last_step > 0.3 and counter < 2000:
         counter += 1
 
-        z_hat = np.zeros(satellites.shape[0])
-        for i in range(satellites.shape[0]):
-            z_hat[i] = np.linalg.norm(x_hat[0:3] - satellites[i]) + x_hat[3]
+        z_hat = np.zeros(satellitePositions.shape[0])
+        for i in range(satellitePositions.shape[0]):
+            z_hat[i] = np.linalg.norm(x_hat[0:3] - satellitePositions[i]) + x_hat[3]
 
         # how much are we off from what we actually measured?
-        delta_z_hat = z - z_hat
+        delta_z_hat = pseudoranges - z_hat
 
         # compute H matrix based on current estimate
-        H = np.zeros((satellites.shape[0], 4))
+        H = np.zeros((satellitePositions.shape[0], 4))
         for i in range(len(H)):
-            H[i, 0:3] = (x_hat[0:3] - satellites[i]) / np.linalg.norm(x_hat[0:3] - satellites[i])
+            H[i, 0:3] = (x_hat[0:3] - satellitePositions[i]) / np.linalg.norm(x_hat[0:3] - satellitePositions[i])
             H[i, 3] = 1
 
         # compute step into the "right" direction
@@ -78,47 +62,47 @@ def calculateUserPosition(satelliteData):
         # update estimate
         x_hat = x_hat + delta_x_hat
 
-    userPositionLlh = ecef2llh(x_hat[0:3])
-    return tor, userPositionLlh, x_hat[3]
+    return x_hat[0:3], x_hat[3]
 
 raw = np.loadtxt("group01_raw.dat")
 raw = raw[raw[:, 1] == 0] # only GPS data
 raw = raw[raw[:, 5] == 0] # only L1 band
 raw = raw[raw[:, 8] >= 3] # only data points with tracking status >= 3
+ephem_from_file = np.loadtxt("group01_ephem.dat")
 
-ephem = np.loadtxt("group01_ephem.dat")
+times = np.zeros(len(np.unique(raw[:, 0])))
+user_positions_ecef = np.zeros((len(np.unique(raw[:, 0])), 3))
+user_clock_errors = np.zeros(len(np.unique(raw[:, 0])))
 
-positions = np.array([])
-
-print(len(np.unique(raw[:,0])), "different times")
-
-for tor in np.unique(raw[:,0]):
+for time_index in range(len(np.unique(raw[:, 0]))):
+    tor = np.unique(raw[:, 0])[time_index]
     print("Time:", tor)
     data_points = raw[raw[:, 0] == tor]
     if len(data_points) < 4: continue # need at least four measurements to determine user position
 
-    tor, userPositionLlh, userClockError = calculateUserPosition(data_points)
+    # now that we see at least four satellites, we can compute an actual user position
+    sv_positions = np.zeros((len(data_points), 3))
+    for row in range(len(data_points)):
+        sv_positions[row] = calculate_sv_pos(tor, data_points[row, 3], data_points[row, 2], ephem_from_file).T
+    user_position_ecef, user_clock_error = calculate_least_squares_solution(data_points[:, 3], sv_positions)
 
-    new_position = np.zeros((1, 5))
-    new_position[0, 0] = tor
-    new_position[0, 1:4] = userPositionLlh
-    new_position[0, 4] = userClockError
-    if len(positions) == 0: positions = new_position
-    else: positions = np.concatenate((positions, new_position), axis=0)
+    times[time_index] = tor
+    user_positions_ecef[time_index] = ecef2llh(user_position_ecef)
+    user_clock_errors[time_index] = user_clock_error
 
 
-plt.scatter(positions[:,2], positions[:,1], c=positions[:,3])
+plt.scatter(user_positions_ecef[:, 1], user_positions_ecef[:, 0], c=user_positions_ecef[:, 2])
 plt.colorbar()
 plt.xlabel("Longitude (degrees)")
 plt.ylabel("Latitude (degrees)")
 plt.title("Estimated positions in lat/lon/height (deg/deg/m)")
 
-# plt.plot(positions[:,0], positions[:,3])
+# plt.plot(times, user_positions_ecef[:,2])
 # plt.ylabel("Height in m")
 # plt.xlabel("Time of Reception")
 # plt.title("Estimated height over time")
 
-# plt.scatter(positions[:,0], positions[:,4])
+# plt.scatter(times, user_clock_errors)
 # plt.ylabel("User clock offset [m]")
 # plt.xlabel("Time of Reception")
 # plt.title("Estimated error over time")
