0e70f2158ebd15bc8c97260244a74b4eabb2a6e5
Leo Vornberger <leo.vornberger@gmx.de>
2024-02-28 12:43:31 +0100

    Implement algorithm from VL 10, slide 15, delivers garbage

--- gnss_position_estimation.py
+++ gnss_position_estimation.py
@@ -64,41 +64,41 @@ def calculate_least_squares_solution(pseudoranges, satellitePositions):
 
     return x_hat[0:3], x_hat[3]
 
-raw = np.loadtxt("group01_raw.dat")
-raw = raw[raw[:, 1] == 0] # only GPS data
-raw = raw[raw[:, 5] == 0] # only L1 band
-raw = raw[raw[:, 8] >= 3] # only data points with tracking status >= 3
-ephem_from_file = np.loadtxt("group01_ephem.dat")
-
-times = np.zeros(len(np.unique(raw[:, 0])))
-user_positions_ecef = np.zeros((len(np.unique(raw[:, 0])), 3))
-user_clock_errors = np.zeros(len(np.unique(raw[:, 0])))
-
-for time_index in range(len(np.unique(raw[:, 0]))):
-    tor = np.unique(raw[:, 0])[time_index]
-    print("Time:", tor)
-    data_points = raw[raw[:, 0] == tor]
-    if len(data_points) < 4: continue # need at least four measurements to determine user position
-
-    # now that we see at least four satellites, we can compute an actual user position
-    sv_positions = np.zeros((len(data_points), 3))
-    pseudoranges = np.zeros(len(data_points))
-    for row in range(len(data_points)):
-        sv_position, updated_pseudorange = calculate_sv_pos(tor, data_points[row, 3], data_points[row, 2], ephem_from_file)
-        sv_positions[row] = sv_position
-        pseudoranges[row] = updated_pseudorange
-    user_position_ecef, user_clock_error = calculate_least_squares_solution(pseudoranges, sv_positions)
-
-    times[time_index] = tor
-    user_positions_ecef[time_index] = ecef2llh(user_position_ecef)
-    user_clock_errors[time_index] = user_clock_error
-
-
-plt.scatter(user_positions_ecef[:, 1], user_positions_ecef[:, 0], c=user_positions_ecef[:, 2])
-plt.colorbar()
-plt.xlabel("Longitude (degrees)")
-plt.ylabel("Latitude (degrees)")
-plt.title("Estimated positions in lat/lon/height (deg/deg/m)")
+# raw = np.loadtxt("group01_raw.dat")
+# raw = raw[raw[:, 1] == 0] # only GPS data
+# raw = raw[raw[:, 5] == 0] # only L1 band
+# raw = raw[raw[:, 8] >= 3] # only data points with tracking status >= 3
+# ephem_from_file = np.loadtxt("group01_ephem.dat")
+#
+# times = np.zeros(len(np.unique(raw[:, 0])))
+# user_positions_ecef = np.zeros((len(np.unique(raw[:, 0])), 3))
+# user_clock_errors = np.zeros(len(np.unique(raw[:, 0])))
+#
+# for time_index in range(len(np.unique(raw[:, 0]))):
+#     tor = np.unique(raw[:, 0])[time_index]
+#     print("Time:", tor)
+#     data_points = raw[raw[:, 0] == tor]
+#     if len(data_points) < 4: continue # need at least four measurements to determine user position
+#
+#     # now that we see at least four satellites, we can compute an actual user position
+#     sv_positions = np.zeros((len(data_points), 3))
+#     pseudoranges = np.zeros(len(data_points))
+#     for row in range(len(data_points)):
+#         sv_position, updated_pseudorange = calculate_sv_pos(tor, data_points[row, 3], data_points[row, 2], ephem_from_file)
+#         sv_positions[row] = sv_position
+#         pseudoranges[row] = updated_pseudorange
+#     user_position_ecef, user_clock_error = calculate_least_squares_solution(pseudoranges, sv_positions)
+#
+#     times[time_index] = tor
+#     user_positions_ecef[time_index] = ecef2llh(user_position_ecef)
+#     user_clock_errors[time_index] = user_clock_error
+#
+#
+# plt.scatter(user_positions_ecef[:, 1], user_positions_ecef[:, 0], c=user_positions_ecef[:, 2])
+# plt.colorbar()
+# plt.xlabel("Longitude (degrees)")
+# plt.ylabel("Latitude (degrees)")
+# plt.title("Estimated positions in lat/lon/height (deg/deg/m)")
 
 # plt.plot(times, user_positions_ecef[:,2])
 # plt.ylabel("Height in m")
--- gnss_velocity_estimation.py
+++ gnss_velocity_estimation.py
@@ -3,7 +3,7 @@ from ephcal import *
 import math
 from matplotlib import pyplot as plt
 from llh2ecef import *
-from gnss_position_estimation import *
+from gnss_position_estimation import calculate_sv_pos, calculate_least_squares_solution
 
 # GPS value for speed of light
 vlight = 299792458.0 # m/s
@@ -17,43 +17,104 @@ raw = raw[raw[:, 5] == 0] # only L1 band
 raw = raw[raw[:, 8] >= 3] # only data points with tracking status >= 3
 
 # make sure the measurements are sorted by time
-for i in range(len(raw)-1):
-    if raw[i+1, 0] - raw[i, 0] >= 0: continue
+for j in range(len(raw) - 1):
+    if raw[j + 1, 0] - raw[j, 0] >= 0: continue
     print("Measurements are not in timely order")
     exit(1)
 
-ephem = np.loadtxt("group01_ephem.dat")
+ephem_from_file = np.loadtxt("group01_ephem.dat")
 
 previous_tor = -1
 current_tor = 0
-satellites_previous = []
-satellites_current = []
+satellite_ids_previous = []
+satellite_ids_current = []
+velocities = np.zeros((len(np.unique(raw[:, 0]))-1, 3))
+time_counter = 0
 for raw_row in range(len(raw)):
     if raw[raw_row, 0] == current_tor: continue
     previous_tor = current_tor
-    current_tor = raw_row[raw_row, 0]
+    current_tor = raw[raw_row, 0]
+    measurements_previous = raw[raw[:, 0] == previous_tor]
+    measurements_current = raw[raw[:, 0] == current_tor]
 
     # step 1: identify satellites common between previous and current tor
-    satellites_previous = satellites_current
-    satellites_current = raw[raw[:, 0] == current_tor][:,2]
-    common_satellite_ids = np.intersect1d(satellites_previous, satellites_current)
+    satellite_ids_previous = satellite_ids_current
+    satellite_ids_current = raw[raw[:, 0] == current_tor][:, 2]
+    common_satellite_ids = np.intersect1d(satellite_ids_previous, satellite_ids_current)
+    if len(common_satellite_ids) < 4: continue # not enough common satellites
 
     # step 2: compute single difference between common satellites
     delta_Phi = np.zeros(len(common_satellite_ids))
-    for i in range(len(common_satellite_ids)):
-        delta_Phi[i] = raw[raw[:, 0] == current_tor and raw[:,2] == common_satellite_ids[i]] - raw[raw[:, 0] == previous_tor and raw[:,2] == common_satellite_ids[i]]
+    for j in range(len(common_satellite_ids)):
+        svid = common_satellite_ids[j]
+        Phi_previous = measurements_previous[measurements_previous[:, 2] == svid][0, 4]
+        Phi_current = measurements_current[measurements_current[:, 2] == svid][0, 4]
+        delta_Phi[j] = Phi_current - Phi_previous
 
     # step 3: calculate unit vectors to satellites at current time
+    satellite_positions_current = np.zeros((len(common_satellite_ids), 3))
+    updated_pseudoranges_current = np.zeros(len(common_satellite_ids))
+    for j in range(len(common_satellite_ids)):
+        svid = common_satellite_ids[j]
+        old_pseudorange = measurements_current[measurements_current[:, 2] == svid][0, 3]
+        sv_position, updated_pseudorange = calculate_sv_pos(current_tor, old_pseudorange, svid, ephem_from_file)
+        satellite_positions_current[j] = sv_position
+        updated_pseudoranges_current[j] = updated_pseudorange
+    user_position_current_ecef, user_clock_error = calculate_least_squares_solution(updated_pseudoranges_current, satellite_positions_current)
     e_current = np.zeros((len(common_satellite_ids), 3))
-    tor, user_position_current, user_clock_error = calculateUserPosition(satellites_current)
-    for i in range(len(common_satellite_ids)):
+    for j in range(len(common_satellite_ids)):
+        e_current[j, :] = (satellite_positions_current[j] - user_position_current_ecef) / np.linalg.norm(satellite_positions_current[j] - user_position_current_ecef)
 
-        e_current[i,:] = 
+    # step 4: Calculate unit vector to satellites at previous time
+    satellite_positions_previous = np.zeros((len(common_satellite_ids), 3))
+    updated_pseudoranges_previous = np.zeros(len(common_satellite_ids))
+    for j in range(len(common_satellite_ids)):
+        svid = common_satellite_ids[j]
+        old_pseudorange = measurements_previous[measurements_previous[:, 2] == svid][0, 3]
+        sv_position, updated_pseudorange = calculate_sv_pos(previous_tor, old_pseudorange, svid, ephem_from_file)
+        satellite_positions_previous[j] = sv_position
+        updated_pseudoranges_previous[j] = updated_pseudorange
+    user_position_previous_ecef, user_clock_error = calculate_least_squares_solution(updated_pseudoranges_previous, satellite_positions_previous)
+    e_previous = np.zeros((len(common_satellite_ids), 3))
+    for j in range(len(common_satellite_ids)):
+        e_previous[j, :] = (satellite_positions_previous[j] - user_position_previous_ecef) / np.linalg.norm(satellite_positions_previous[j] - user_position_previous_ecef)
 
-# plt.scatter(velocities[:,2], velocities[:,1], c=velocities[:,3])
-# plt.colorbar()
-# plt.xlabel("Longitude (degrees)")
-# plt.ylabel("Latitude (degrees)")
-# plt.title("Estimated positions in lat/lon/height (deg/deg/m)")
-# plt.show()
+    # step 5: compute Doppler correction term
+    svdoppler = np.zeros(len(common_satellite_ids))
+    for j in range(len(svdoppler)):
+        for dim in range(3):
+            svdoppler += e_current[j, dim] * satellite_positions_current[j, dim] # TODO: should we really use these satellite positions?
+            svdoppler -= e_previous[j, dim] * satellite_positions_previous[j, dim]
+
+    # step 6: compute the Geometry correction term
+    dgeom = np.zeros(len(common_satellite_ids))
+    for j in range(len(dgeom)):
+        for dim in range(3):
+            svdoppler += e_previous[j, dim] * user_position_previous_ecef[dim]
+            svdoppler -= e_current[j, dim] * user_position_previous_ecef[dim]
+
+    # step 7: make Doppler- and Geometry adjustments
+    z = np.zeros(len(common_satellite_ids))
+    for j in range(len(z)):
+        z[j] = delta_Phi[j] - svdoppler[j] - dgeom[j]
+
+    # step 8: Set up H
+    H = np.zeros((len(common_satellite_ids), 4))
+    for j in range(len(H)):
+        H[j, 0:3] = -e_current[j]
+        H[j, 3] = 1
+
+    # step 9: perform least squares
+    delta_x = np.linalg.inv(H.T @ H) @ H.T @ z
+    delta_x = delta_x / (current_tor - previous_tor)
+    delta_x = delta_x[0:3]
+    print(delta_x, np.linalg.norm(delta_x))
+    velocities[time_counter, :] = delta_x
+    time_counter += 1
+
+plt.scatter(velocities[:, 0], velocities[:, 1])
+plt.xlabel("velocity x (m/s)")
+plt.ylabel("velocity y (m/s)")
+plt.title("Estimated velocity")
+plt.show()
 
